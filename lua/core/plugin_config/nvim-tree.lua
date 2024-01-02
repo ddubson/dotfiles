b1da8c90c5907761cadf7ef109162046c2a4ea43
Dmitriy Dubson <d.dubson@gmail.com>
2024-01-01 19:29:46 -0500

    Update nvim-tree config

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,9 +1,14 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.termguicolors = true
+
 require("nvim-tree").setup({
   git = {
     enable = true,
+  },
+  renderer = {
+    group_empty = true,
   }
 })
 
